2e5c5dd852157299317a4c506166d58f0cad207d
Pavan Prakash K <pavancodes05@gmail.com>
2025-01-09 11:46:22 +0530

    Create Skeleton

---
+++ main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+void displayGameboard();
+char determineWinner(char board[3][3]);
+
+int main() {
+    return 0;
+};
+
+void displayGameboard() {
+
+};
+
+char determineWinner(char board[3][3]) {
+
+};
