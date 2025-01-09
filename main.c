3493727e0e08534ee83d47f0dc39ba1165693be2
Pavan Prakash K <pavancodes05@gmail.com>
2025-01-09 12:01:10 +0530

    Create and Display Game Board

--- main.c
+++ main.c
@@ -1,13 +1,29 @@
 #include <stdio.h>
 
-void displayGameboard();
+void displayGameboard(char board[3][3]);
 char determineWinner(char board[3][3]);
 
 int main() {
+    char board[3][3] = {
+        {1, 2, 3},
+        {4, 5, 6},
+        {7, 8, 9}
+    };
+
+    displayGameboard(board);
     return 0;
 };
 
-void displayGameboard() {
+void displayGameboard(char board[3][3]) {
+    int i, j;
+    for(i = 0;i < 3; i++) {
+        for (j = 0; j < 3; j++) {
+            printf("|");
+            printf(" %d ", board[i][j]);
+            printf("|");
+        }
+        printf("\n");
+    }   
 
 };
 
